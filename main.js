311a463a47ffbc66d3b16d5826496f44f00d8203
Ivan Huynh <32465307+huynh-ivan@users.noreply.github.com>
2023-08-29 21:48:16 -0400

    Set up html/css/js files for the project
    Also, add some pseudocode in main.js to outline the project

---
+++ main.js
@@ -0,0 +1,20 @@
+//represent the container in javascript
+
+
+//represent a square div in javascript
+
+
+//add eventListener for mouseenter event
+
+
+//add eventListener for the mouseleave event
+
+
+//add eventlistener for the mouseover event
+
+/*
+Questions:
+1. how do I get the window to display 16 squares in a grid on load?
+2. what is `window` in relation to document?
+
+ */
