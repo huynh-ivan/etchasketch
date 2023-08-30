6f777aa82043fedf46deecfab1cf96886b0f3dc5
Ivan Huynh <32465307+huynh-ivan@users.noreply.github.com>
2023-08-29 22:49:51 -0400

    Add a few more square elements
    It appears that adding the squares one by one was the only way to achieve distinct squares on the page.
    
    I find that hard to be true, but I suppose I should look into this more.

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-//represent the container in javascript
+//Add eventlistener for when the webpage loads
+const rootElement = document.documentElement;
+//site.addEventListener('load', drawSquares())
 
+
+//represent the container in javascript
+const container = document.querySelector('.container');
 
 //represent a square div in javascript
+const square1 = document.createElement('div');
+square1.classList.add('square');
+
+const square2 = document.createElement('div');
+square2.classList.add('square');
+
+const square3 = document.createElement('div');
+square3.classList.add('square');
+
+container.appendChild(square1);
+container.appendChild(square2);
+container.appendChild(square3);
+//write a funciton to draw 1 square
 
 
 //add eventListener for mouseenter event
@@ -14,7 +32,19 @@
 
 /*
 Questions:
-1. how do I get the window to display 16 squares in a grid on load?
-2. what is `window` in relation to document?
+- how do I get the window to display 16 squares in a grid on load?
+    - probably adding an eventlistener to the `body` tag listening for the load event
+    - I suspect I will need to call a function to draw exactly 16 squares
+    - I remember there being something on mdn about drawing circles. Maybe that's a clue?
+- for all the squares, it feels like they need to have classes
+    - this is so I can toggle the classes based on eventlisteners
+    - there will need to be some sort of start class (regular color) and then the change class (the color to change to)
+    - on mouseenter, the function should toggle the change class on AND the start class off
+    - on mouseleave, the function should toggle the change class off AND the start class on
+- what is the best way to draw 16 squares?
+    - do I loop through something somehow?
+    - how would I loop through an append()
+
+
 
  */
