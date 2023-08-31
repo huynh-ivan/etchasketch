da7cbffa5d03c749acfe257e6f72443617f44e60
Ivan Huynh <32465307+huynh-ivan@users.noreply.github.com>
2023-08-30 21:31:06 -0400

    Add CSS to display all 256 squares a grid
    Also made the innerText set to the counter variable so I can quickly view at a glance how many squares there are

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ const rootElement = document.documentElement;
 const container = document.querySelector('.container') //select the container in javascript
 
 const grid = document.createElement('div')//Create a grid div
+grid.classList.add('grid');
 
 function makeGrid(rows, columns) {
-    for (let i = 0; i <= rows*columns; i++) {
+    for (let i = 1; i <= rows*columns; i++) {
         const box = document.createElement('div')//Create a box div
         box.id = i; 
+        box.innerText = i;
         box.classList.add('box');
         grid.appendChild(box);
         container.appendChild(grid);
