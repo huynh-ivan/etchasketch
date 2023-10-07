26d6ee378e4f0e1e80794be54730650abbaf898d
Ivan Huynh <32465307+huynh-ivan@users.noreply.github.com>
2023-10-07 17:57:13 -0400

    Updated both css + js files to use css grid
    There were things I believe(?) I tried but didn't work out until I rewrote everything. My guess is that copy/pasting affected the callstack somehow.

--- main.js
+++ main.js
@@ -1,49 +1,35 @@
 
-function makeGrid(rows, columns) {
-    //select the container div in javascript
-    const container = document.querySelector('.container')
-    container.style.height = "960px";
-    container.style.width = "960px";
-    container.style.display = "grid";
-    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
-    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
-    container.style.position = 'relative';
-    container.style.margin = "64px";
+//select the container div in javascript
+const grid = document.querySelector('.grid')
+grid.classList.add('grid')
 
 
-    //use a loop to create boxes based on the # of rows * columns
-    for (let i = 1; i <= rows*columns; i++) {
+function makeGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+  for (let i = 0; i < size * size; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.id = i;
+    gridItem.textContent = i;
+    gridItem.classList.add('gridItem');
         
-        //Create a box div
-        const box = document.createElement('div')
-        box.id = i; 
-        box.textContent = i;
-        box.style.display = "flex";
-        box.style.height = `calc(100% / columns)`
-        box.style.width = `calc(100% / rows)`
-        box.style.backgroundColor = 'darkgrey'
-        box.style.justifyContent = 'center';
-        box.style.alignItems = 'center';
-        box.style.textAlign = 'center';
-        box.style.padding ='4px';
+    gridItem.addEventListener('mouseenter', toggleHover);
+    gridItem.addEventListener('mouseleave', toggleHover);
+    
+    function toggleHover() {
+      gridItem.classList.toggle('hover');
+    };
 
-        //Add Eventlisteners
-        box.addEventListener('mouseenter', enableHover);
-        box.addEventListener('mouseleave', disableHover);
-
-        //Write function to change background color of a box 
-        function enableHover() {
-            box.classList.add('hover');            
-        };
-
-        //Write function to remove the background color from a box
-        function disableHover() {
-           box.classList.remove('hover');
-        };
-
-        //Append Box to Container
-        container.appendChild(box);
+    grid.appendChild(gridItem);
     };
 };
 
-makeGrid(16,16);
+
+
+
+makeGrid(23);
+
+
+
+
