c02a6fb4a9f2345895b6110105f49a4d51146235
Ivan Huynh <32465307+huynh-ivan@users.noreply.github.com>
2023-10-07 14:27:05 -0400

    Removed css and put it inline into js
    In cleaning up the css, I also made the container itself a grid, removing the unncessary `grid` class

--- main.js
+++ main.js
@@ -1,43 +1,49 @@
-//Add eventlistener for when the webpage loads
-const rootElement = document.documentElement;
-//rootElement.addEventListener('load', makeSquares())
-
-const container = document.querySelector('.container') //select the container in javascript
-
-const grid = document.createElement('div')//Create a grid div
-grid.classList.add('grid');
 
 function makeGrid(rows, columns) {
+    //select the container div in javascript
+    const container = document.querySelector('.container')
+    container.style.height = "960px";
+    container.style.width = "960px";
+    container.style.display = "grid";
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+    container.style.position = 'relative';
+    container.style.margin = "64px";
+
+
+    //use a loop to create boxes based on the # of rows * columns
     for (let i = 1; i <= rows*columns; i++) {
-        const box = document.createElement('div')//Create a box div
+        
+        //Create a box div
+        const box = document.createElement('div')
         box.id = i; 
-        box.innerText = i;
-        box.classList.add('box');
-        grid.appendChild(box);
-        container.appendChild(grid);
+        box.textContent = i;
+        box.style.display = "flex";
+        box.style.height = `calc(100% / columns)`
+        box.style.width = `calc(100% / rows)`
+        box.style.backgroundColor = 'darkgrey'
+        box.style.justifyContent = 'center';
+        box.style.alignItems = 'center';
+        box.style.textAlign = 'center';
+        box.style.padding ='4px';
 
-        box.addEventListener('mouseenter', enableHover)
-        box.addEventListener('mouseleave', disableHover)
+        //Add Eventlisteners
+        box.addEventListener('mouseenter', enableHover);
+        box.addEventListener('mouseleave', disableHover);
 
+        //Write function to change background color of a box 
         function enableHover() {
             box.classList.add('hover');            
         };
 
+        //Write function to remove the background color from a box
         function disableHover() {
-          box.classList.remove('hover');
+           box.classList.remove('hover');
         };
+
+        //Append Box to Container
+        container.appendChild(box);
     };
 };
 
 makeGrid(16,16);
-
-//Make a box
-
-/*
-
-Calculate the size of a box based on the row + column values
-- can this be done with javascript?
-- can I write a function to resize a box based on the input values?
-
-
-*/
